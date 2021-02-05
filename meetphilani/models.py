61c8aaaa1f5af57f4f2c439075cb6bd722d60829
PhilaniAntony <philaniantonydlamini@gmail.com>
2021-02-05 15:00:37 +0200

    models setup

--- meetphilani/models.py
+++ meetphilani/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
 # Create your models here.
+
+
+class AboutMe(models.Model):
+    title = models.CharField(max_length=50)
+    descrption = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Technology(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
+class Project(models.Model):
+
+    title = models.CharField(max_length=50)
+    image = models.ImageField()
+    description = models.TextField()
+    technology = models.ManyToManyField(Technology)
+    source_code = models.URLField()
+    deployed_version = models.URLField()
+
+    def __str__(self):
+        return self.title
+
+
+class Message(models.Model):
+    name = models.CharField(max_length=150)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Collaborations(models.Model):
+    name = models.CharField(max_length=150)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
